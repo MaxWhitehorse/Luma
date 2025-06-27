7a2ec3821e72fe9ef991b73f0f8b4974047fc15f
Popplit <sashadesh9@gmail.com>
2025-06-27 15:07:43 +0700

    Добавил Классы для Luma LumaNegativeTest и LumaPositiveTest

---
+++ src/test/java/info/esoft/qa/luma_negative_tests/LumaNegativeTest.java
@@ -0,0 +1,9 @@
+package info.esoft.qa.luma_negative_tests;
+import org.junit.jupiter.api.Test;
+public class LumaNegativeTest {
+
+    @Test
+    public void lumaNegativeTest() {
+
+    }
+}
---
+++ src/test/java/info/esoft/qa/luma_positive_tests/LumaPositiveTest.java
@@ -0,0 +1,9 @@
+package info.esoft.qa.luma_positive_tests;
+import org.junit.jupiter.api.Test;
+public class LumaPositiveTest {
+
+    @Test
+    public void lumaPositiveTest() {
+
+    }
+}
